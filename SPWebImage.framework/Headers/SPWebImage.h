24290933557fecfe4fd64a248af7e9e02daf82d8
SandyParmar <sandeep.parmar.ios@gmail.com>
2017-02-17 16:10:45 +0530

    Add files via upload

---
+++ SPWebImage.framework/Headers/SPWebImage.h
@@ -0,0 +1,19 @@
+//
+//  SPWebImage.h
+//  SPWebImage
+//
+//  Created by Sandeep on 17/02/17.
+//  Copyright © 2017 Sandeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPWebImage.
+FOUNDATION_EXPORT double SPWebImageVersionNumber;
+
+//! Project version string for SPWebImage.
+FOUNDATION_EXPORT const unsigned char SPWebImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPWebImage/PublicHeader.h>
+
+
